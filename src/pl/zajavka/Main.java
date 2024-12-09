abee7decea105dea60de2c5fb05607a4c4f71620
Karol Janowski <104912717+Karol-Janowski@users.noreply.github.com>
2024-12-09 12:26:55 +0100

    added notes about Spring

--- src/pl/zajavka/Main.java
+++ src/pl/zajavka/Main.java
@@ -3,5 +3,8 @@ package pl.zajavka;
 public class Main {
     public static void main(String[] args) {
         // Spring
+        // convention over configuration
+        // dependency injection
+        // inversion of control
     }
 }
