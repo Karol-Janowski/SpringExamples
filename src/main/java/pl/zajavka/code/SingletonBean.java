f8938789b0df919edd8c51385119afbe9ac748be
Karol Janowski <104912717+Karol-Janowski@users.noreply.github.com>
2024-12-13 11:12:36 +0100

    added some lombock adnotations

--- src/main/java/pl/zajavka/code/SingletonBean.java
+++ src/main/java/pl/zajavka/code/SingletonBean.java
@@ -1,19 +1,18 @@
 package pl.zajavka.code;
 
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalTime;
-
+@Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class SingletonBean {
 
 
     private ObjectFactory<PrototypeBean> prototypeBean;
-
-    public SingletonBean(ObjectFactory<PrototypeBean> prototypeBean) {
-        this.prototypeBean = prototypeBean;
-        System.out.println("Calling singletonBean()");
-    }
 
     public ObjectFactory<PrototypeBean> callPrototype() {
         System.out.println("Calling prototype: " + LocalTime.now());
